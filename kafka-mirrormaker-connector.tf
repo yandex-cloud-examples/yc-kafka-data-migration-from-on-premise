43a58a7b153229021dad0d8a88c487670dcb0eda
Nadezhda-Subotka <subotka-n@yandex-team.ru>
2024-06-20 20:30:53 +0300

    YCDOCIO-3571: Updated Terraform manifests

--- kafka-mirrormaker-connector.tf
+++ kafka-mirrormaker-connector.tf
@@ -1,35 +1,45 @@
 # Infrastructure for Yandex Cloud Managed Service for Apache Kafka® and MirrorMaker connector
 #
-# RU: https://cloud.yandex.ru/docs/managed-kafka/tutorials/mirrormaker-unmanaged-topics
-# EN: https://cloud.yandex.com/en/docs/managed-kafka/tutorials/mirrormaker-unmanaged-topics
+# RU: https://yandex.cloud/ru/docs/managed-kafka/tutorials/kafka-connectors
+# EN: https://yandex.cloud/en/docs/managed-kafka/tutorials/kafka-connectors
 #
-# Set the following settings:
+# Configure the parameters of the source and target clusters:
 
 locals {
-  zone_a_v4_cidr_blocks    = "10.1.0.0/16"               # Set the CIDR block for subnet in the ru-central1-a availability zone.
-  zone_b_v4_cidr_blocks    = "10.2.0.0/16"               # Set the CIDR block for subnet in the ru-central1-b availability zone.
-  zone_c_v4_cidr_blocks    = "10.3.0.0/16"               # Set the CIDR block for subnet in the ru-central1-c availability zone.
-  source_user              = ""                          # Source cluster user name.
-  source_password          = ""                          # Source cluster user password.
-  source_alias             = "source"                    # Specify prefix for the source cluster.
-  source_bootstrap_servers = "<FQDN1>:9091,<FQDN2>:9091" # Specify bootstrap servers to connect to cluster.
-  target_user              = ""                          # Target cluster user name.
-  target_password          = ""                          # Target cluster user password.
-  target_alias             = "target"                    # Specify prefix for the target cluster.
-  topics_prefix            = "data.*"                    # Specify topics that must be migrated.
-  kafka_version            = "2.8"                       # Specify version of Apache Kafka® for the cluster.
-  sasl_mechanism           = "SCRAM-SHA-512"             # Specify encryption algorythm for username and password.
-  security_protocol        = "SASL_SSL"                  # Specify connection protocol for the MirrorMaker connector.
+  source_user              = ""                          # Source cluster username
+  source_password          = ""                          # Source cluster user's password
+  source_alias             = "source"                    # Prefix for the source cluster
+  source_bootstrap_servers = "<FQDN1>:9091,<FQDN2>:9091" # Bootstrap servers to connect to cluster
+  target_password          = ""                          # Target cluster user's password
+  target_alias             = "target"                    # Prefix for the target cluster
+  topics_prefix            = "data.*"                    # Topics that must be migrated
+  kafka_version            = ""                          # Desired version of Apache Kafka®. For available versions, see the documentation main page: https://yandex.cloud/en/docs/managed-kafka/.
+
+  # The following settings are predefined. Change them only if necessary.
+  network_name          = "network"       # Name of the network
+  subnet_a_name         = "subnet-a"      # Name of the subnet in the ru-central1-a availability zone
+  subnet_b_name         = "subnet-b"      # Name of the subnet in the ru-central1-b availability zone
+  subnet_d_name         = "subnet-d"      # Name of the subnet in the ru-central1-d availability zone
+  zone_a_v4_cidr_blocks = "10.1.0.0/16"   # CIDR block for subnet in the ru-central1-a availability zone
+  zone_b_v4_cidr_blocks = "10.2.0.0/16"   # CIDR block for subnet in the ru-central1-b availability zone
+  zone_d_v4_cidr_blocks = "10.3.0.0/16"   # CIDR block for subnet in the ru-central1-d availability zone
+  target_cluster        = "kafka-cluster" # Name of the Apache Kafka® target cluster
+  target_connector      = "replication"   # Name of the Apache Kafka® connector for the target cluster
+  target_user           = "admin-cloud"   # Username of the Apache Kafka® for the target cluster
+  sasl_mechanism        = "SCRAM-SHA-512" # Encryption algorythm for username and password
+  security_protocol     = "SASL_SSL"      # Connection protocol for the MirrorMaker connector
 }
+
+# Network infrastructure
 
 resource "yandex_vpc_network" "network" {
   description = "Network for the Managed Service for Apache Kafka® cluster and VM"
-  name        = "network"
+  name        = local.network_name
 }
 
 resource "yandex_vpc_subnet" "subnet-a" {
   description    = "Subnet in ru-central1-a availability zone"
-  name           = "subnet-a"
+  name           = local.subnet_a_name
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
@@ -37,18 +47,18 @@ resource "yandex_vpc_subnet" "subnet-a" {
 
 resource "yandex_vpc_subnet" "subnet-b" {
   description    = "Subnet in ru-central1-b availability zone"
-  name           = "subnet-b"
+  name           = local.subnet_b_name
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = [local.zone_b_v4_cidr_blocks]
 }
 
-resource "yandex_vpc_subnet" "subnet-c" {
-  description    = "Subnet in ru-central1-c availability zone"
-  name           = "subnet-c"
-  zone           = "ru-central1-c"
+resource "yandex_vpc_subnet" "subnet-d" {
+  description    = "Subnet in ru-central1-d availability zone"
+  name           = local.subnet_d_name
+  zone           = "ru-central1-d"
   network_id     = yandex_vpc_network.network.id
-  v4_cidr_blocks = [local.zone_c_v4_cidr_blocks]
+  v4_cidr_blocks = [local.zone_d_v4_cidr_blocks]
 }
 
 resource "yandex_vpc_default_security_group" "security-group" {
@@ -72,23 +82,24 @@ resource "yandex_vpc_default_security_group" "security-group" {
   }
 }
 
+# Infrastructure for the Managed Service for Apache Kafka® cluster
+
 resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
   description        = "Managed Service for Apache Kafka® cluster"
-  name               = "kafka-cluster"
+  name               = local.target_cluster
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.network.id
   security_group_ids = [yandex_vpc_default_security_group.security-group.id]
 
   config {
-    brokers_count    = 1
-    version          = local.kafka_version
-    zones            = ["ru-central1-a"]
-    unmanaged_topics = true # Topic management via the Admin API.
+    brokers_count = 1
+    version       = local.kafka_version
+    zones         = ["ru-central1-a"]
     kafka {
       resources {
         disk_size          = 10 # GB
         disk_type_id       = "network-hdd"
-        resource_preset_id = "s2.micro" # 2 vCPU, 8 GB RAM.
+        resource_preset_id = "s2.micro" # 2 vCPU, 8 GB RAM
       }
       kafka_config {
         auto_create_topics_enable = true
@@ -96,21 +107,27 @@ resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
     }
   }
 
-  user {
-    name     = local.target_user
-    password = local.target_password
-    permission {
-      topic_name = "*"
-      role       = "ACCESS_ROLE_ADMIN"
-    }
+  depends_on = [
+    yandex_vpc_subnet.subnet-a
+  ]
+}
+
+# User of the Managed Service for Apache Kafka® cluster
+resource "yandex_mdb_kafka_user" "user" {
+  cluster_id = yandex_mdb_kafka_cluster.kafka-cluster.id
+  name       = local.target_user
+  password   = local.target_password
+  permission {
+    topic_name = "*"
+    role       = "ACCESS_ROLE_ADMIN"
   }
 }
 
+# MirrorMaker connector of the Managed Service for Apache Kafka® cluster
 resource "yandex_mdb_kafka_connector" "connector" {
-  description = "MirrorMaker connector"
-  cluster_id  = yandex_mdb_kafka_cluster.kafka-cluster.id
-  name        = "replication"
-  tasks_max   = 3
+  cluster_id = yandex_mdb_kafka_cluster.kafka-cluster.id
+  name       = local.target_connector
+  tasks_max  = 3
   connector_config_mirrormaker {
     topics             = local.topics_prefix
     replication_factor = 1
