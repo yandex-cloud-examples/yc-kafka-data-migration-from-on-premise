6172cb42ff5c428b3bd436584b0b7f6ae51b620f
szenkov454 <szenkov454@yandex-team.ru>
2026-03-31 19:46:07 +0700

    YCDOCIO-3937: TF manifest updates

--- kafka-mirror-maker.tf
+++ kafka-mirror-maker.tf
@@ -5,23 +5,32 @@
 #
 # Configure the parameters of the Managed Service for Apache Kafka® cluster and Virtual Machine:
 
+# local variables
 locals {
-  kf_password     = "" # Apache Kafka® admin's password
-  kf_version      = "" # Desired version of Apache Kafka®. For available versions, see the documentation main page: https://yandex.cloud/en/docs/managed-kafka/.
+  # The following settings are to be specified by the user. Change them as you wish.
+
+  # Settings for the Managed Service for Apache Kafka® cluster
+  kf_cluster_name = "" # Name of the Apache Kafka® cluster
+  kf_password     = "" # User's password in Apache Kafka® cluster
+
+  # Settings for the Compute Cloud VM
   image_id        = "" # Public image ID from https://yandex.cloud/en/docs/compute/operations/images-with-pre-installed-software/get-list
   vm_username     = "" # Username to connect to the routing VM via SSH. Images with Ubuntu Linux use the `ubuntu` username by default.
   vm_ssh_key_path = "" # Path to the SSH public key for the routing VM. Example: "~/.ssh/key.pub".
 
   # The following settings are predefined. Change them only if necessary.
-  network_name          = "network"       # Name of the network
-  subnet_name           = "subnet-a"      # Name of the subnet
-  zone_a_v4_cidr_blocks = "10.1.0.0/16"   # CIDR block for subnet in the ru-central1-a availability zone
-  kf_cluster_name       = "kafka-cluster" # Name of the Apache Kafka® cluster
-  kf_username           = "admin-cloud"   # Username of the Apache Kafka® cluster
+
+  # Settings for the Managed Service for Apache Kafka® cluster
+  kf_username = "admin-cloud" # Username of the Apache Kafka® cluster
+  kf_version  = "3.9"         # Desired version of Apache Kafka®. For available versions, see the documentation main page: https://yandex.cloud/en/docs/managed-kafka/.
+
+  # Settings for the Virtual Private Cloud network infrastructure
+  network_name          = "network"     # Name of the network
+  subnet_name           = "subnet-a"    # Name of the subnet
+  zone_a_v4_cidr_blocks = "10.1.0.0/16" # CIDR block for subnet in the ru-central1-a availability zone
 }
 
 # Network infrastructure
-
 resource "yandex_vpc_network" "network" {
   description = "Network for the Managed Service for Apache Kafka® cluster and VM"
   name        = local.network_name
@@ -30,8 +39,8 @@ resource "yandex_vpc_network" "network" {
 resource "yandex_vpc_subnet" "subnet-a" {
   description    = "Subnet in the ru-central1-a availability zone"
   name           = local.subnet_name
-  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
+  zone           = "ru-central1-a"
   v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
 }
 
@@ -64,7 +73,6 @@ resource "yandex_vpc_default_security_group" "security-group" {
 }
 
 # Infrastructure for the Managed Service for Apache Kafka® cluster
-
 resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
   description        = "Yandex Managed Service for Apache Kafka® cluster"
   name               = local.kf_cluster_name
@@ -80,7 +88,7 @@ resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
       resources {
         disk_size          = 10 # GB
         disk_type_id       = "network-hdd"
-        resource_preset_id = "s2.micro"
+        resource_preset_id = "s2.micro" # 2 vCPU, 8GB RAM
       }
       kafka_config {
         auto_create_topics_enable = true
@@ -93,7 +101,7 @@ resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
   ]
 }
 
-# User of the Managed service for the Apache Kafka® cluster
+# User of the Managed service for Apache Kafka® cluster
 resource "yandex_mdb_kafka_user" "mkf-user" {
   cluster_id = yandex_mdb_kafka_cluster.kafka-cluster.id
   name       = local.kf_username
@@ -105,11 +113,11 @@ resource "yandex_mdb_kafka_user" "mkf-user" {
 }
 
 # VM infrastructure
-
 resource "yandex_compute_instance" "vm-mirror-maker" {
   description = "VM in Yandex Compute Cloud"
   name        = "vm-mirror-maker"
   platform_id = "standard-v3" # Intel Ice Lake
+  zone        = "ru-central1-a"
 
   resources {
     cores  = 2
@@ -124,7 +132,7 @@ resource "yandex_compute_instance" "vm-mirror-maker" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet-a.id
-    nat       = true # Required for connection from the Internet
+    nat       = true # Assigns public IP-address for connection from the Internet
   }
 
   metadata = {
